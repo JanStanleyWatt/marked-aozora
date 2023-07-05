af78a30006593eef894fdd9f6b0a7ad1396ddc4b
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2023-07-06 01:49:06 +0900

    Update `debug` in `scripts` to add env for debug

--- src/debug.ts
+++ src/debug.ts
@@ -7,8 +7,11 @@ marked.use({
     extensions: [aozoraRuby],
 });
 
-const markdown: string = "これは実に**素敵**《すてき》で完璧《**かんぺき**》だ";
+const markdown = "これは実に｜素敵《すてき》で完璧《**かんぺき**》だ";
+const env = process.env;
 
-console.dir(marked.lexer(markdown), { depth: null });
-console.log("");
+if (env.MARKED_AOZORA_DEBUG_VERBOSE === "1") {
+    console.dir(marked.lexer(markdown), { depth: null });
+    console.log("");
+}
 console.log(marked.parse(markdown));
