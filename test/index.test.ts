45f9d70275a645b66b8191f28366f120cf090ecd
Jan Stanley Watt <12198769+JanStanleyWatt@users.noreply.github.com>
2023-07-07 21:07:20 +0900

    Add test patterns

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,7 +8,6 @@ test("delim_normal", () => {
         extensions: [aozoraRuby({ useRpTag: false, useSutegana: false })],
     });
 
-test("delim_normal", () => {
     const html = marked.parseInline(
         "このエクステンションは｜素晴らしい《パーフェクトだ》な"
     );
@@ -173,5 +172,133 @@ test("hiragana", () => {
 
     expect(html).toBe(
         "That<ruby>かくちょうきのう<rt>エクステンション</rt></ruby>は、<ruby>いちばん<rt>ｲﾁﾊﾞﾝ</rt></ruby>素晴らしいな"
+    );
+});
+
+test("option_default", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby()],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは超優秀《ちょうゆうしゅう》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby>超優秀<rt>ちょうゆうしゅう</rt></ruby>である"
+    );
+});
+
+test("option_useRpTag", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: true, useSutegana: false })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは超優秀《ちょうゆうしゅう》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby>超優秀<rp>（</rp><rt>ちょうゆうしゅう</rt><rp>）</rp></ruby>である"
+    );
+});
+
+test("option_useSutegana", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: false, useSutegana: true })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは超優秀《ちょうゆうしゅう》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby>超優秀<rt>ちようゆうしゆう</rt></ruby>である"
+    );
+});
+
+test("option_both", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: true, useSutegana: true })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは超優秀《ちょうゆうしゅう》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby>超優秀<rp>（</rp><rt>ちようゆうしゆう</rt><rp>）</rp></ruby>である"
+    );
+});
+
+test("em_in_rt", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: false, useSutegana: false })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは超優秀《*ちょうゆうしゅう*》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby>超優秀<rt><em>ちょうゆうしゅう</em></rt></ruby>である"
+    );
+});
+
+test("em_in_ruby", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: false, useSutegana: false })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは｜*超優秀*《ちょうゆうしゅう》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby><em>超優秀</em><rt>ちょうゆうしゅう</rt></ruby>である"
+    );
+});
+
+test("em_both", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: false, useSutegana: false })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは *超優秀《ちょうゆうしゅう》* である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは <em><ruby>超優秀<rt>ちょうゆうしゅう</rt></ruby></em> である"
+    );
+});
+
+test("ruby_nest", () => {
+    marked.use({
+        mangle: false,
+        headerIds: false,
+        extensions: [aozoraRuby({ useRpTag: false, useSutegana: false })],
+    });
+
+    const html = marked.parseInline(
+        "このエクステンションは超優秀《ちょうゆうしゅう《パーフェクト》》である"
+    );
+
+    expect(html).toBe(
+        "このエクステンションは<ruby>超優秀<rt><ruby>ちょうゆうしゅう<rt>パーフェクト</rt></ruby></rt></ruby>である"
     );
 });
